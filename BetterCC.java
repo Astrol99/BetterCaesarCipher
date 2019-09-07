de67a40e898ad165736a5a1a07b6e864634189e4
Dav2006 <dngames2006@gmail.com>
2019-09-07 13:23:15 -0500

    Reseted files and changed name

---
+++ BetterCC.java
@@ -0,0 +1,11 @@
+import java.util.Scanner;
+import java.util.Random;
+
+//WORK ON BRUTEFORCE FEATURE
+
+public class BetterCC{
+    
+    public static void main(String[] args) {
+        System.out.println("x");
+    }
+}    
