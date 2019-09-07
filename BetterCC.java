435c8de50d1d3ac9e0c2b986465c93dd40193a76
Dav2006 <dngames2006@gmail.com>
2019-09-07 18:00:55 -0500

    Added main class

--- BetterCC.java
+++ BetterCC.java
@@ -3,12 +3,22 @@ import java.util.Random;
 
 public class BetterCC{
     
+    // Parameters for cipher
     String plainText;
     String cipherText;
+    int key;
+
+    // Main cipher base
     static String alphabetUpper = "ABCDEFGHIJKLMNOPQRSTUVWXYZ";
     static String alphabetLower = alphabetUpper.toLowerCase();
 
+    // Main cipher class
+    public CaesarCipher(String input, String output, int secretKey){
+        plainText = input;
+        cipherText = output;
+        key = secretKey;
+    }
     public static void main(String[] args) {
-        System.out.println();
+        
     }
 }    
